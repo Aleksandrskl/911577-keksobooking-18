452f89a766cca9cf74773816a0e39ee9b5ee628d
Aleksandr <sklarsanja@gmail.com>
2019-09-18 23:09:35 +0300

    на проверку 1-й пунктзадачи

---
+++ js/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+{
+  author: {
+    avatar: 'img / avatars / user01.png';
+  },
+  offer: {
+    title: 'Уютное гнездышко для молодоженов';
+    address: '600, 350';
+    price: 5200;
+    type: 'flat';
+    rooms: 2;
+    guests: 3;
+    checkin: '14: 00';
+      checkout: '10: 00';
+    features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+      description: 'Великолепная квартира - студия в центре Токио.Подходит как туристам, так и бизнесменам.Квартира полностью укомплектована и недавно отремонтирована.';
+        photos: [ 'http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
+  },
+
+  location: {
+    "600"
+    "350"
+  }
+}
+
+//убераю класс .map--faded.
